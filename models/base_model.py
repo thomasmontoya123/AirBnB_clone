135a3a5c9acbe1e8bdb302d634d09baf7e82d368
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-11-07 15:33:32 -0500

    task-4

--- models/base_model.py
+++ models/base_model.py
@@ -12,11 +12,25 @@ class BaseModel:
     Defines all common attributes/methods for other classes
     '''
 
-    def __init__(self, id=None, created_at=None, update_at=None):
-        if id is None:
-            self.id = str(uuid.uuid4())
+    def __init__(self, *args, **kwargs):
+        '''
+        Constructor
+        '''
+        args_len = len(args)
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        if args_len == 0:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key == "__class__":
+                    continue
+                else:
+                    setattr(self, key, value)
 
     def __str__(self):
         return "[{}] ({}) {}".format(BaseModel.__name__,
@@ -29,5 +43,5 @@ class BaseModel:
         dict_to_return = self.__dict__
         dict_to_return["__class__"] = BaseModel.__name__
         dict_to_return["created_at"] = str(datetime.isoformat(self.created_at))
-        dict_to_return["update_at"] = str(datetime.isoformat(self.update_at))
+        dict_to_return["updated_at"] = str(datetime.isoformat(self.updated_at))
         return dict_to_return
