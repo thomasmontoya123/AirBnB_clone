3306a76af2d9ad8c6adcecb498e4486388c33bb9
guxal <jonathanacp93@gmail.com>
2019-11-12 18:00:46 +0000

    working int the file

---
+++ tests/test_console.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+'''test for console'''
+import unittest
+
+
+class TestConsole(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """setup for the test"""
+        cls.consol = HBNBCommand()
+    
+    def test_pep8_console(self):
+        pass
+
+    def test_docstrings_in_console(self):
+        pass
+
+    def 
+        pass
+
+    def
+        pass
+
+    def
+        pass
+
+    def
+        pass
+
+    def
+        pass
+
+    def
+        pass
+
+    def
+        pass
+
+    def
+        pass
+
+    def
+        pass
+
+    def
+        pass
+
+    
