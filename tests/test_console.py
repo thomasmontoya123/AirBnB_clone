8a72b6a661383c542b33be7526d3dc00cb830656
guxal <jonathanacp93@gmail.com>
2019-11-12 18:52:14 +0000

    test case methods

--- tests/test_console.py
+++ tests/test_console.py
@@ -6,43 +6,39 @@ import unittest
 class TestConsole(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        """setup for the test"""
+        '''setup for the test'''
         cls.consol = HBNBCommand()
     
     def test_pep8_console(self):
+        '''test pep8 console.py'''
         pass
 
     def test_docstrings_in_console(self):
+        '''test docstrings console.py'''
         pass
 
-    def 
+    def test_quit(self):
+        '''test quit command input'''
         pass
 
-    def
+    def test_create(self):
+        '''test create command input'''
         pass
 
-    def
+    def test_destroy(self):
+        '''test destroy command input'''
         pass
 
-    def
+    def test_all(self):
+        '''test all command input'''
         pass
 
-    def
+    def test_update(self):
+        '''test update command input'''
         pass
 
-    def
-        pass
-
-    def
-        pass
-
-    def
-        pass
-
-    def
-        pass
-
-    def
+    def test_emptyline(self):
+        '''test empty line input'''
         pass
 
     
