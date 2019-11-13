c576edfec3509b2de55ff699bca385a250a501c2
guxal <jonathanacp93@gmail.com>
2019-11-13 00:38:28 +0000

    add test_models methods

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,4 +4,30 @@ import unittest
 
 
 class TestUser(unittest.TestCase):
-    pass
+    
+    def setUpClass(cls):
+        pass
+
+    def teardown(cls):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_pep8_User(self):
+        pass
+
+    def test_checking_for_docstring_User(self):
+        pass
+
+    def test_is_subclass_User(self):
+        pass
+
+    def test_attribute_types_User(self):
+        pass
+
+    def test_save_User(self):
+        pass
+
+    def test_to_dict_User(self):
+        pass
