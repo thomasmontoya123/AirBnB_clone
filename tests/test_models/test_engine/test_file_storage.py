0327ffd33617d343ebb596e8afc282edbe89646a
guxal <jonathanacp93@gmail.com>
2019-11-13 14:16:45 +0000

    add test_file_storage methods

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''test for Storage Class'''
+import unittest
+
+
+class TestFileStorage():
+
+    def setUpClass(cls):
+        pass
+
+    def teardown(cls):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_pep8_FileStorage(self):
+        pass
+
+    def test_all(self):
+        pass
+
+    def test_new(self):
+        pass
+
+    def test_reload_filestorage(self):
+        pass
